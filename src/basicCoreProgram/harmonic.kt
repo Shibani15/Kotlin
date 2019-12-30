262c2ed600fc4cfb90e17604e151c2155edd2fbf
Shibani15 <mishrashibani384@gmail.com>
2019-12-30 13:08:57 +0530

    adding harmonic.kt

---
+++ src/basicCoreProgram/harmonic.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    var value1: doublei = 1
+    var value2: intn = 5
+    var value3: doubles = 0.0
+    val value = arrayOf(1, 2, 3, 4, 5)
+    for (value in ArrayList.indices)
+        s = S + 1 / intArrayOf(i)
+    return s;
+}
+
+
+
+
